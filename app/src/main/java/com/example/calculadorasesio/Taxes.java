37c64c3e18d800f7c94750cce61616e31d203bad
Julián Serio <julian_serio@yahoo.com.ar>
2023-01-28 16:02:19 -0300

    Interface modification, internet usage.

--- app/src/main/java/com/example/calculadorasesio/Taxes.java
+++ app/src/main/java/com/example/calculadorasesio/Taxes.java
@@ -1,17 +1,15 @@
 package com.example.calculadorasesio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 public class Taxes extends AppCompatActivity {
     private EditText et;
-    private TextView tvImpPais,tvImpGanancias,tvSinImp,tvTotal;
+    private TextView tvImpPais,tvImpGanancias,tvSinImp,tvTotal,tvDolar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +21,7 @@ public class Taxes extends AppCompatActivity {
         tvImpPais = findViewById(R.id.tvImpuestoPais);
         tvImpGanancias = findViewById(R.id.tvImpuestoGanancias);
         tvTotal = findViewById(R.id.tvTotal);
-
+        tvDolar = findViewById(R.id.tvDolar);
     }
 
     public void irAMain (View view){
@@ -32,24 +30,25 @@ public class Taxes extends AppCompatActivity {
     }
 
     public void calcular (View view){
-        int precioDolarHoy = 190;
+        int precioDolarHoy = 185;
+        tvDolar.setText(String.valueOf(precioDolarHoy));
         int pImP = 30;
         int pImG = 45;
         float totalImpPais,totalSinImp,totalImpGan,total;
         float dolars = Float.parseFloat(et.getText().toString());
-        if (dolars == 0) {
+        if (dolars == 0)  {
             tvTotal.setText("ERROR!");
         }
         else{
             totalSinImp = dolars * precioDolarHoy;
             totalImpPais = ((dolars / 100) * pImP) * precioDolarHoy;
             totalImpGan = ((dolars / 100) * pImG) * precioDolarHoy;
-            total = totalSinImp + totalImpPais + totalImpGan;
+            total = (totalSinImp + totalImpPais + totalImpGan);
 
             tvSinImp.setText(String.valueOf(totalSinImp));
             tvImpPais.setText(String.valueOf(totalImpPais));
             tvImpGanancias.setText(String.valueOf(totalImpGan));
-            tvTotal.setText(String.valueOf(total));
+            tvTotal.setText((String.valueOf(total)));
         }
     }
 
